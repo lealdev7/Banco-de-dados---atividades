5fb1fc1dad7e8e82589455e8405eddfef6e1ea56
João Leal <joaolealdev@gmail.com>
2026-02-13 22:46:33 -0300

    Add files via upload

---
+++ Atividade 1 - BD.sql
@@ -0,0 +1,17 @@
+CREATE TABLE focos_calor 
+id SERIAL PRIMARY KEY,
+estado VARCHAR(50),
+bioma VARCHAR(50),
+data_ocorrencia DATE
+)
+
+INSERT INTO focos_calor (estado, bioma, data_ocorrencia) VALUES
+('Amazonas', 'Amazônia',
+'2025-02-01’),
+('Mato Grosso', 'Cerrado',
+'2025-02-03’),
+('Pará', 'Amazônia',
+'2025-02-05');
+
+
+SELECT * FROM focos_calor;
